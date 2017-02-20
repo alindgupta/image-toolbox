9214774f591524b511c28fde0793c9db70c9a426
Alind Gupta <alindgupta19@gmail.com>
2017-02-20 13:23:21 -0500

    Refactored code, added function help text

--- autoLength.m
+++ autoLength.m
@@ -1,3 +1,46 @@
+function [m, sd] = autoLengths(minlen, maxlen)
+% Calculate lengths of objects in fluorescence images automatically
+% by providing a background value
+% - minlen : minimum cutoff for lengths
+% - maxlen : maximum cutoff for lengths
+
+    if nargin < 2:
+            minlen = 5      % smallest size acceptable (number of pixels)
+            maxlen = 100    % largest size acceptable (number of pixels)
+    end
+
+    % read image file and display
+    filename = uigetfile;
+    im = imread(filename);
+    im = rgb2gray(im);
+    figure;
+    imshow(im);
+
+    temp = input('Please zoom to a single object in focus');
+    temp = input('Please select background pixels');
+    [x,y] = ginput();
+    prof = improfile(im, x, y);
+    profMean = ceil(mean(prof));
+
+    imbw = im > profMean;
+    smallestObjArea = ceil(pi * ((minlen-1) / 2) ** 2);  % smallest object
+    imopen = bwareaopen(imbw, smallestObjArea);
+
+    x = regionprops(imopen, 'MajorAxisLength');
+    x = cell2mat(struct2cell(x))(:);
+
+    y = x(x < maxlen && x > minlen);
+    [m, sd] = [mean(y), std(y)]
+    % dlmwrite('means.csv', y, '-append');
+
+end
+
+    
+
+%{
+
+% Original code
+
 clear all
 close all
 clc
@@ -11,7 +54,6 @@ temp = input('Please zoom to single centriole');
 [x,y] = ginput();
 prof = improfile(im, x, y);
 profMean = mean(prof);
-
 ceilProfMean = ceil(profMean);
 
 imbw = im > ceilProfMean;
@@ -25,3 +67,5 @@ y = x(x < 13);
 mean(y)
 std(y)
 %dlmwrite ('meanswt.csv', y, '-append');
+%}
+
--- dotLength.m
+++ dotLength.m
@@ -1,79 +1,85 @@
-%% clear
-clc
-clear all
-close all
+function dotLength(im1, varargin)
+%   Calculate lengths of objects supervised by user input
+%   Firstly, user selects background pixel value cutoff (averaged)
+%   Then, user manually inputs (using mouse) line segment across objects
+%   Finally, a scale is selected for pixel to length conversion
+%   The program writes lengths of all measured objects to a csv file
+%
+% - im1 is the image of interest
+% - im2 (out of varargin) may be useful for orientation, not required though
+%
 
-%% get images
+    %% clear
+    clc
+    clear all
+    close all
 
-im1 = imread('T00001C01Z001.tif');
-im2 = imread('T00001C02Z001.tif');
-im1 = rgb2gray(im1);
-im2 = rgb2gray(im2);
-im = imfuse(imadjust(im1), imadjust(im2));
-
-figure, imshow(im);
-temp = input('Please adjust the zoom to your liking. Hit a key when done.');
-
-
-
-%% get background
-
-
-temp = input('Please select a line with cutoff for background levels.');
-[a, b] = ginput();
-backgroundVec = improfile(im2, a, b);
-backgroundInt = mean(backgroundVec);
-
-
-%% main loop
-
-% container array
-tempArr = zeros(1,200);         % stores output from improfile
-tempArrLogical = zeros(1,200);  % stores a logical, >background value
-finalLengths = zeros(100, 1);   % stores final lengths from tempArr
-
-length = 0;
-
-userInput = 1;
-i = 1;
-
-while userInput ~= 0
-    
-    try
-        [x,y] = ginput();
-        tempArr = improfile(im2, x, y);
-    
-        tempArrayLogical = tempArr > backgroundInt
-    
-        length = max( find(diff(tempArrayLogical)==-1) - find(diff(tempArrayLogical)==1) )
-        finalLengths(i) = length;
-         i = i+1;
-    
-        userInput = input('1 to go, 0 to stop');  
-    
-    catch ME
-        continue
+    %% file IO
+    if nargin == 0
+        error('Require at least one image file!');        
     end
-     
+
+    if length(varargin) > 1
+        disp('Only one additional image file is supported, truncating to first...');
+        im2 = varargin{1};
+    end
+
+    if length(varargin) == 0
+        im2 = zeros(size(im1));
+    end
+
+    im1 = rgb2gray(im1);
+    im2 = rgb2gray(im2);
+    im = imfuse(imadjust(im1), imadjust(im2));
+
+    figure, imshow(im);
+    temp = input('Please adjust the zoom to your liking. Hit a key when done');
+
+
+    %% get background
+    temp = input('Please select background pixels');
+    [a, b] = ginput();
+    backgroundVec = improfile(im1, a, b);
+    backgroundInt = mean(backgroundVec);
+
+
+    %% main loop
+
+    % container array
+    tempArr = zeros(1,200);         % stores output from improfile
+    tempArrLogical = zeros(1,200);  % stores a logical, >background value
+    finalLengths = zeros(100, 1);   % stores final lengths from tempArr
+
+    length = 0;
+
+    userInput = 1;
+    i = 1;
+
+    while userInput ~= 0
+    
+        try
+            [x,y] = ginput();
+            tempArr = improfile(im2, x, y);
+    
+            tempArrayLogical = tempArr > backgroundInt
+    
+            length = max( find(diff(tempArrayLogical)==-1) - find(diff(tempArrayLogical)==1) )
+            finalLengths(i) = length;
+            i = i+1;
+    
+            userInput = input('1 to go, 0 to stop');  
+    
+        catch ME
+            continue
+        end 
+    end
+
+    temp = input('Go to the scale bar and enter scale ref');
+    [x,y] = ginput();
+    delX = x(end)-x(1);
+    finalLengths(finalLengths==0)=[];
+    finalLengths = finalLengths*(temp/delX);
+
+    dlmwrite ('mean.csv', finalLengths, '-append');
+
 end
-
-temp = input('Go to the scale bar and enter scale ref');
-[x,y] = ginput();
-delX = x(end)-x(1);
-finalLengths(finalLengths==0)=[];
-finalLengths = finalLengths*(temp/delX);
-
-
-dlmwrite ('mean.csv', finalLengths, '-append');
-
-
-
-
-
-
-
-
-
-
-
-
